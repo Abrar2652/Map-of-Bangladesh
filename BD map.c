5b11d9f460476057b3640781213411d425bf6c6a
Md. Abrar Jahin <56369280+Abrar2652@users.noreply.github.com>
2020-12-16 02:48:50 +0600

    Update BD map.c

--- BD map.c
+++ BD map.c
@@ -5,23 +5,20 @@ int main()
     char str[] = "ED..GDAD..DLEB..COCC..CV..FS..HQ..JN..MP..Go..Cr..Cq..Cp..Fk..Jf..J`..I`..H`ID..J^HE..K^FG..N[ABCG..L`CG..MTBT..MUCS..NTDBCBAJ..NUBBHI..OTMI..OROI..OGDCSI..PE[I..RC[I..rBDB..rB..sB..tB";
     int c, i;
     for (i = 0; i < strlen(str); i++){
-        c = str[i]-64;//for E, c=69-64=5 and i = 0 so will print space for 5 times
-        if(str[i]==46){
+        c = str[i] - 64;//for E, c=69-64=5 and i = 0 so will print space for 5 times
+        if(str[i] == 46){
             putchar(10);//print newline if find '.'
             ++i;
         }
-        else if (i%2 == 0){
-            while(c != 0 && i != strlen(str)/2){
+        else if (i % 2 == 0){
+            while(c != 0){
                 putchar(32);//for even, print space
                 c--;
             }
         }
-        else if (i == strlen(str)/2)
-        {
-            printf("Tribute//From//Abrar");
-        }
+
         else{
-            while(c!=0 && i != strlen(str)/2){
+            while(c != 0){
                 putchar('!');//for odd, print !
                 c--;
             }
